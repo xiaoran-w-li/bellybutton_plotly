fa8f53d33f31fb2be12137fe9260a47cba2ff897
Xiaoran Li <74216423+xiaoran-w-li@users.noreply.github.com>
2021-05-02 17:42:15 -0400

    all things working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -18,7 +18,10 @@ updatePage()
 
 function optionChanged(value) {
 DemographicInfo(value)
+charts(value)
 }
+
+
 
 function DemographicInfo(userid) {
     d3.json("samples.json").then((data)=> {
@@ -29,20 +32,60 @@ function DemographicInfo(userid) {
     selectedbox.html("")
     // calling metadata 
     var demodata = data.metadata
-    var filtereddemodata = demodata.filter(x =>  x.id = userid) [0];
+    console.log(demodata)
+    var filtereddemodata = demodata.filter(x =>x.id == userid) [0];
 //    for loop to append selectedbox
     Object.entries(filtereddemodata).forEach(([key,value])=>{
-        selectedbox.append("p").text(`${key} : ${value}`)
+        selectedbox.append("option").text(`${key} : ${value}`)
     }
     )
 })
 };
 
-function barchart(userid) {
+function charts(userid) {
     d3.json("samples.json").then((data)=> {
         console.log(data)
     // calling metadata 
     var bardata = data.samples
-    var filteredbardata = bardata.filter(x =>  x.id = userid) [0];
+    console.log(bardata)
+    var filteredbardata = bardata.filter(x =>x.id == userid) [0];
+    // barchart = filteredbardata.slice(0, 10);
 
+    // Reverse the array due to Plotly's defaults
+    // barchart = barchart.reverse();
+    
+    
+    
+    var trace1 = [{
+      x: filteredbardata.sample_values.slice(0, 10).reverse(),
+      y: filteredbardata.otu_ids.map(id =>`otu${id}`).slice(0, 10).reverse(),
+      text: filteredbardata.otu_labels.slice(0,10).reverse(),
+      name: "Belly Button Bar",
+      type: "bar",
+      orientation: "h"
+    }];
+    layout = {
+        title : {text: "Top 10 Belly Button Bacteria"},
+        xaxis : {title: "sample values"}
+    }
+    Plotly.newPlot("bar", trace1,layout);
+
+    var trace2 = [{
+        x: filteredbardata.otu_ids,
+        y: filteredbardata.sample_values,
+        text: filteredbardata.otu_labels,
+        name: "Belly Button Bubble",
+        mode: "markers",
+        marker: { 
+            color: filteredbardata.otu_ids,
+            size: filteredbardata.sample_values,
+        }
+      }];
+      layout = {
+          title : {text: "Belly Button Bacteria Frequency "},
+          xaxis : {title: "otu ids"},
+          yaxis : {title: "sample values"},
+      }
+      Plotly.newPlot("bubble", trace2,layout);
 })};
+
